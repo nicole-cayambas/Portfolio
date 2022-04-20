c1f9965fed22f1b7ae2816999c68ba52b2eb1479
nicole-cayambas <55810654+nicole-cayambas@users.noreply.github.com>
2022-04-21 00:37:50 +0800

    added more content

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ function changeColor(){
 
 const header = document.getElementById("header")
 window.addEventListener("scroll", function(){
-    if(window.scrollY > 100 && window.scrollY < 500){
+    if(window.scrollY > 100){
         header.style.backgroundColor = "#C8C8C8"
         header.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)"
         header.style.transition = "all 0.5s"
-    } else if(window.scrollY > 500 || window.scrollY < 100){
+    } else {
         header.style.backgroundColor = "transparent"
         header.style.boxShadow = "none"
         header.style.transition = "all 0.5s"
