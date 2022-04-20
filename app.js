8d22f595bc8ba811841ecce64fb4515df4da3aae
Nicole Cayambas <55810654+nicole-cayambas@users.noreply.github.com>
2022-04-20 12:50:07 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function hire(){
+    let hireBtn = document.getElementById("hireBtn")
+    console.log("hire clicked!");
+}
+function changeColor(){
+    let arrowDownLink = document.getElementById("arrow-down-a")
+    arrowDownLink.style.color = "rgb("
+    + Math.floor(Math.random()*255)
+    + ","
+    + Math.floor(Math.random()*255)
+    + ","
+    + Math.floor(Math.random()*255)
+    + ")"
+}
+
+const header = document.getElementById("header")
+window.addEventListener("scroll", function(){
+    if(window.scrollY > 100 && window.scrollY < 500){
+        header.style.backgroundColor = "#C8C8C8"
+        header.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)"
+        header.style.transition = "all 0.5s"
+    } else if(window.scrollY > 500 || window.scrollY < 100){
+        header.style.backgroundColor = "transparent"
+        header.style.boxShadow = "none"
+        header.style.transition = "all 0.5s"
+    }
+})
